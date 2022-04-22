7aecbfa8b605f87a445b70f7fe8bdb98a8b1a6b0
NicatMirzaev <nicatmirzoev111@gmail.com>
2022-04-22 19:37:46 +0400

    updates

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,15 +1,26 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('users', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     username: {
       type: DataTypes.STRING,
+      allowNull: false,
       unique: true,
     },
     email: {
       type: DataTypes.STRING,
+      allowNull: false,
       unique: true,
     },
     password: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
   });
 
--- backend/src/schema/index.js
+++
@@ -1,5 +0,0 @@
-import hi from './hi';
-
-export default [
-  hi,
-];
